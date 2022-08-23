8eceaf463758d528513a55fc8af7f963514b6b86
Marcela <arqmrcl@gmail.com>
2022-08-22 23:41:48 -0300

    req10

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+function SearchBar() {
+    return (
+        <section>
+            <input></input>
+        </section>
+    )
+}
--- src/context/RecipesContext.js
+++ src/context/RecipesContext.js
@@ -1,5 +1,7 @@
 import { createContext } from 'react';
 
-const RecipesContext = createContext();
+const RecipesContext = createContext(
+    
+);
 
 export default RecipesContext;
