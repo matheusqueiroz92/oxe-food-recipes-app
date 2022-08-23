1f8f06c3fae454e7a07770cd55007bb634eaba83
adyluna <adylunaleite@gmail.com>
2022-08-23 15:12:37 -0300

    Ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,33 @@ import React from 'react';
 import ProfileIcon from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
 
-const Header = ({ title, profile, search }) => (
-  <div>
-    <h1 data-testid="page-title">{title}</h1>
-    { profile && <img
-      alt="profile-pic"
-      src={ ProfileIcon }
-      data-testid="profile-top-btn"
-    />}
-    { search && <img
-      alt="search-pic"
-      src={ SearchIcon }
-      data-testid="search-top-btn"
-    />}
-  </div>
-);
+const Header = ({ title, profile, search, history }) => {
+  const redirectToProfile = () => {
+    console.log('Entrei');
+    history.push('/profile');
+  };
+  return (
+    <div>
+      <h1 data-testid="page-title">{title}</h1>
+      {
+        profile && <input
+          alt="profile"
+          type="image"
+          src={ ProfileIcon }
+          data-testid="profile-top-btn"
+          onClick={ redirectToProfile }
+        />
+      }
+      { search && <input
+        alt="search"
+        type="image"
+        src={ SearchIcon }
+        data-testid="search-top-btn"
+        onClick={ redirectToProfile }
+      />}
+    </div>
+  );
+};
 
 Header.defaultProps = {
   profile: false,
@@ -25,6 +37,9 @@ Header.defaultProps = {
 };
 
 Header.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
   profile: PropTypes.bool,
   search: PropTypes.bool,
   title: PropTypes.string.isRequired,
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,10 +1,17 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Header from '../components/Header';
 
-const Foods = () => (
+const Foods = ({ history }) => (
   <div>
-    <Header title="Foods" profile search />
+    <Header title="Foods" profile search history={ history } />
   </div>
 );
+
+Foods.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Foods;
