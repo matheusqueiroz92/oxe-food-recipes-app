3eb0002f563d974689c96c63a511ed53b2a2e398
Patricia Correia <pncorreia25@gmail.com>
2022-08-28 19:58:05 -0300

    req37 a 39

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -8,10 +8,22 @@ function RecipeInProgress() {
   const { id } = useParams();
   const { pathname } = useLocation();
   const compare = pathname.includes('drink') ? 'drinks' : 'foods';
-  const [recipeDetails, setRecipeDetails] = useState([]);
-  const [ingredientsList, setIngredientsList] = useState([]);
+  const [handleRecipe, sethandleRecipe] = useState([]);
+  const [checkedList, setCheckedList] = useState([]);
+  const [checked, setChecked] = useState([]);
   const ENDPOINTFOOD = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
   const ENDPOINTDRINK = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+  useEffect(() => {
+    localStorage.setItem('inProgressRecipes', {
+      cocktails: {
+        [id]: [],
+      },
+      meals: {
+        [id]: [],
+      },
+    });
+  }, []);
 
   useEffect(() => {
     const fetchEndpoint = async (URL, compareData) => {
@@ -19,20 +31,20 @@ function RecipeInProgress() {
       const response = await request.json();
       const data = response[compareData][0];
 
-      setRecipeDetails(data);
+      sethandleRecipe(data);
 
-      const ingredientFilter = object.entries(data)
+      const ingredientFilter = Object.entries(data)
         .filter((ingredient) => ingredient[0].includes('strIngredient')
       && ingredient[1] !== null && ingredient[1] !== '');
 
-      const measuredFilter = object.entries(data)
-        .filter((measured) => measured[0].includes('strIngredient')
+      const measuredFilter = Object.entries(data)
+        .filter((measured) => measured[0].includes('strMeasure')
         && measured[1] !== null && measured[1] !== '');
 
       const ingredientList = ingredientFilter
         .map((ingredient, index) => (measuredFilter[index] === null
           ? `${ingredient[1]}` : `${ingredient[1]} - ${measuredFilter[index][1]}`));
-      setIngredientsList(ingredientList);
+      setCheckedList(ingredientList);
     };
 
     if (compare === 'foods') {
@@ -43,37 +55,65 @@ function RecipeInProgress() {
     }
   }, []);
 
+  const handleCheck = ({ target }) => {
+    let updatedList = [...checked];
+    if (target.checked) {
+      updatedList = [...checked, target.value];
+    } else {
+      updatedList.splice(checked.indexOf(target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  const isChecked = (ingredient) => (checked.includes(ingredient)
+    ? 'checked-item' : 'not-checked-ingredient');
+
   return (
     <div>
-      <img
-        src={ compare === 'foods'
-          ? recipeDetails.strMealThumb : recipeDetails.strDrinkThumb }
-        alt={ compare === 'foods'
-          ? recipeDetails.strMealThumb : recipeDetails.strDrinkThumb }
-        data-testid="recipe-photo"
-      />
+
       <h1 data-testid="recipe-title">
-        { compare === 'food'
-          ? recipeDetails.strMeal : recipeDetails.strDrink }
+        {compare === 'foods'
+          ? handleRecipe.strMeal : handleRecipe.strDrink}
       </h1>
+      <div>
+        <img
+          src={ compare === 'foods'
+            ? handleRecipe.strMealThumb : handleRecipe.strDrinkThumb }
+          alt={ compare === 'foods'
+            ? handleRecipe.strMealThumb : handleRecipe.strDrinkThumb }
+          data-testid="recipe-photo"
+        />
+      </div>
+      <h4 data-testid="recipe-category">{ handleRecipe.strCategory }</h4>
+      <div className="list-container">
+        {
+          checkedList.length > 0
+          && checkedList.map((ingredient, index) => (
+            <label
+              key={ index }
+              htmlFor={ index }
+              data-testid={ `${index}-ingredient-step` }
+            >
+              <input
+                value={ ingredient }
+                id={ index }
+                type="checkbox"
+                onChange={ handleCheck }
+              />
+              <span className={ isChecked(ingredient) }>{ingredient}</span>
+            </label>))
+        }
+      </div>
+      <p data-testid="instructions">{handleRecipe.strInstructions}</p>
+
       <button type="button" data-testid="share-btn">
         <img className="icon" src={ shareIcon } alt="" />
       </button>
+
       <button type="button" data-testid="favorite-btn">
         <img className="icon" src={ whiteHeartIcon } alt="" />
       </button>
-      <p data-testid="recipe-category">
-        { ingredientsList.length > 0 && ingredientsList.map((ingredient, index) => (
-          <label key={ index } htmlFor={ index }>
-            <input
-              id={ index }
-              type="checkbox"
-              data-testid={ `${index}-ingredient-step` }
-            />
-            {ingredient}
-          </label>))}
-      </p>
-      <p data-testid="instructions">{ recipeDetails.strInstructions }</p>
+      <p data-testid="recipe-category" />
       <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
     </div>
   );
