4243f8fcaed6f1667476e82cca9317ad42857a66
adyluna <adylunaleite@gmail.com>
2022-09-04 22:01:51 -0300

    Req 45 ok

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
 
 // const toGo = [{
 //   id: 17203,
-//   type: 'bebida',
+//   type: 'drink',
 //   nationality: '',
 //   category: 'Ordinary Drink',
 //   alcoholicOrNot: 'Alcoholic',
@@ -11,36 +12,61 @@ import Header from '../components/Header';
 //   image: 'https://www.thecocktaildb.com/images/media/drink/apneom1504370294.jpg',
 //   doneDate: '23/06/2020',
 //   tags: ['IBA', 'ContemporaryClassic'],
+// },
+// {
+//   id: '52771',
+//   type: 'food',
+//   nationality: 'Italian',
+//   category: 'Vegetarian',
+//   alcoholicOrNot: '',
+//   name: 'Spicy Arrabiata Penne',
+//   image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+//   doneDate: '23/06/2020',
+//   tags: ['Pasta', 'Curry'],
 // }];
 
 // localStorage.setItem('doneRecipes', JSON.stringify(toGo));
 
 const DoneRecipes = () => {
   const getDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-  const renderDoneRecipes = getDoneRecipes.map((elem, index) => (
-    <div key={ index }>
-      <img
-        alt={ elem.name }
-        src={ elem.image }
-        data-testid={ `${index}-horizontal-image` }
-      />
-      <p data-testid={ `${index}-horizontal-top-text` }>{ elem.category }</p>
-      <p data-testid={ `${index}-horizontal-name` }>{ elem.name }</p>
-      <p data-testid={ `${index}-horizontal-done-date` }>{ elem.doneDate }</p>
-      <p data-testid={ `${index}-horizontal-share-btn` }>Test</p>
-      { elem
-        .tags
-        .map((
-          tag,
-        ) => (
-          <p
-            key={ index }
-            data-testid={ `${index}-${tag}-horizontal-tag` }
+  const renderDoneRecipes = getDoneRecipes.map((elem, index) => {
+    const isFood = elem.type === 'food';
+    return (
+      <div key={ index }>
+        <img
+          alt={ elem.name }
+          src={ elem.image }
+          data-testid={ `${index}-horizontal-image` }
+        />
+        { isFood
+          && <p
+            data-testid={ `${index}-horizontal-top-text` }
           >
-            { tag }
-          </p>)) }
-    </div>
-  ));
+            { `${elem.nationality} - ${elem.category}` }
+             </p> }
+        <p data-testid={ `${index}-horizontal-name` }>{ elem.name }</p>
+        <p data-testid={ `${index}-horizontal-done-date` }>{ elem.doneDate }</p>
+        <img
+          alt={ elem.name }
+          src={ shareIcon }
+          data-testid={ `${index}-horizontal-share-btn` }
+          style={ { width: 20, height: 20 } }
+        />
+        { elem
+          .tags
+          .map((
+            tag,
+            i,
+          ) => (
+            <p
+              key={ i }
+              data-testid={ `${index}-${tag}-horizontal-tag` }
+            >
+              { tag }
+            </p>)) }
+      </div>
+    );
+  });
   return (
     <div>
       <div>
